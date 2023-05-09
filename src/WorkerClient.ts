d58006c89bc3e50dc6d0894d14ddb3874cde1b63
irishcarbomb777 <danielclannan@gmail.com>
2023-05-09 14:23:59 -0400

    Initial commit

---
+++ src/WorkerClient.ts
@@ -0,0 +1,29 @@
+import {rxToTx} from '../../shared/txRx'
+
+export class WorkerClient {
+    actionTable: Record<string, (payload: any)=>void> = {}
+    constructor(public worker: Worker, actions: Record<string, (payload: any)=>void>){
+        this.addActions(actions)
+        this.worker.onmessage = (event: MessageEvent<{action: string, payload: any}>) => {
+            const {action, payload} = event.data
+            if (this.actionTable[action]){
+                this.actionTable[action](payload)
+            } 
+        } 
+    }
+    addAction = (action: string, callback: (payload: any)=>void) => {
+        this.on(rxToTx(action), callback)
+    }
+
+    addActions = (actions: Record<string, (payload: any)=>void>) => {
+        for (const [action, callback] of Object.entries(actions)){
+            this.on(rxToTx(action), callback)
+        }
+    }
+    sendMessage = async (action: string, payload: Record<string, any>) => {
+        this.worker.postMessage({action, payload})
+    }
+    private on = (action: string, callback: (payload: any)=>void) => {
+        this.actionTable[action] = callback
+    }
+}
